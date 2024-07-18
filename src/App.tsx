6adad23a69aaf071ab1dcb556b59d515d19ede19
Ezebed <obedvillegas.02@gmail.com>
2024-07-18 15:22:33 -0400

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import "@/assets/css/App.css"
 import { useEffect, useRef } from "react"
 
-import HolaMundo from "@/components/hola"
-
 import useGetPokemons from "@/hooks/useGetPokemons"
 
 import { IPokemonEntry } from "./utils/Interfaces/IPokemon"
@@ -13,11 +11,6 @@ function App() {
   const {loading, pokemonEntries, hasMore, morePokemons }  = useGetPokemons()
   const loadRef = useRef<HTMLDivElement>(null)
 
-  
-  function hola(){
-    
-    console.log(pokemonEntries)
-  }
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
@@ -42,24 +35,21 @@ function App() {
 
       <h1 className="capitalize text-7xl font-bold my-20" >infinite scroll</h1>
 
-      <div className="px-4 py-2 my-2 flex gap-2 justify-center items-center" >
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif" alt="pikachu animated" className="size-10"/>
-        <p className="font-bold capitalize" >loading...</p>
-      </div>
-
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-2"> 
         {pokemonEntries.map((data:IPokemonEntry,index:number) => ( <PokeCard key={index} {...data} /> ))}
         
       </div>
       
-      <button disabled={loading} onClick={hola}>api</button>
-      <button disabled={!hasMore} onClick={morePokemons}>more</button>
       { hasMore && 
-        <div ref={loadRef} className="px-4 py-2 my-2 flex gap-2 justify-center items-center" >
+        <div ref={loadRef} className="px-4 py-2 mt-16 flex gap-2 justify-center items-center" >
           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif" alt="pikachu animated" className="size-10"/>
           <p className="font-bold capitalize" >loading...</p>
         </div>
       }
+
+      <div className="px-2.5 py-1 rounded-lg border-2 fixed left-2 bottom-2">
+        <p className="font-bold ">Developed by Obed Villegas</p>
+      </div>
     </>
   )
 }
